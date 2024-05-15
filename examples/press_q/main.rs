8bc0bacbd163af6c4d164564ade768f7911d95e1
NormanStansfield <NormanStansfield@users.noreply.github.com>
2024-05-15 21:52:34 +0900

    first step & press q

---
+++ examples/press_q/main.rs
@@ -0,0 +1,19 @@
+// 標準ライブラリの読み込み
+use std::io::{self, Read};
+
+// main 関数
+fn main() {
+    // STDINからbyte列を読み込み
+    for input_byte in io::stdin().bytes() {
+        // 読み込んだbyteをcharに変換して束縛
+        let output_char = input_byte.unwrap() as char;
+
+        // 読み込んだ値を出力
+        println!("{}", output_char);
+
+        // qが入力されたらbreak
+        if output_char == 'q' {
+            break;
+        }
+    }
+}
