d1e24fc5d2459919730741fa60ab064bbead92df
NormanStansfield <NormanStansfield@users.noreply.github.com>
2025-07-14 02:40:42 +0900

    add test_notice_provider

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,4 @@ pub(crate) const STEP: usize = 3;
 // pub(crate) const ORIGIN_X: u16 = 10;
 // pub(crate) const ORIGIN_Y: u16 = 2;
 pub(crate) const ADDRESS_WIDTH: usize = 8;
+pub(crate) const LIMIT: u8 = 2; // しばらく表示するためのアクション猶予回数
--- src/notice_provider.rs
+++ src/notice_provider.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
-use std::os::linux::raw::stat;
 
+use crate::constants;
 use crate::interfaces::Notice;
 use crate::interfaces::NoticeProviderTrait;
 
@@ -11,26 +11,17 @@ pub(crate) struct NoticeProvider {
     cache: String,
 }
 
-// // ステータス伝達
-// pub(crate) struct Notice {
-//     count: Cell<u8>,
-//     notice: RefCell<VecDeque<String>>,
-//     cache: RefCell<String>,
-// }
-
 impl NoticeProviderTrait for NoticeProvider {
     fn new() -> Self {
         Self {
             count: 0,
             queue: VecDeque::new(),
             cache: String::with_capacity(0),
-            // ::new(),
         }
     }
 
     // メッセージキューへ追加
     fn add(&mut self, notice: Notice) {
-        // let Notice(state) = state;
         let message = notice.into_inner();
 
         self.queue.push_back(message);
@@ -38,52 +29,40 @@ impl NoticeProviderTrait for NoticeProvider {
 
     // メッセージ取得
     fn get_notice(&mut self) -> Notice {
-        const LIMIT: u8 = 2; // しばらく表示するためのアクション猶予回数
-
         match self.count {
             0 => {
                 if let Some(message) = self.queue.pop_front() {
-                    // self.cache = format!("{state}");
                     self.cache = message;
                     self.count = 1;
                 } else {
                     self.cache.clear();
                 }
             }
-            x if x > LIMIT => {
-                // self.count.set(0);
+            x if x >= constants::LIMIT => {
                 self.count = 0;
             }
             x => {
-                // self.count.set(x + 1);
                 self.count += 1;
             }
         }
 
-        // self.cache.borrow().clone()
         Notice::new(self.cache.clone())
     }
+}
 
-    //  fn pop_front(&self) -> String {
-    //     const LIMIT: u8 = 2; // しばらく表示するためのアクション猶予回数
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::constants::LIMIT;
 
-    //     match self.count.get() {
-    //         0 => {
-    //             if let Some(state) = self.notice.borrow_mut().pop_front() {
-    //                 *self.cache.borrow_mut() = format!(" {state} ");
-    //                 self.count.set(1);
-    //             } else {
-    //                 self.cache.borrow_mut().clear();
-    //             }
-    //         }
-    //         x if x > LIMIT => {
-    //             self.count.set(0);
-    //         }
-    //         x => {
-    //             self.count.set(x + 1);
-    //         }
-    //     }
-
-    //     self.cache.borrow().clone()
-    // }
+    #[test]
+    fn test_notice_provider() {
+        let mut notice_provider = NoticeProvider::new();
+        let notice = "TestMessage".to_string();
+        notice_provider.add(Notice::new(notice.clone()));
+        (0..=LIMIT).for_each(|_| {
+            let res = notice_provider.get_notice();
+            assert_eq!(res.into_inner(), notice);
+        });
+    }
 }
